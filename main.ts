1b6021ea932e197f228c43141bfb416dfcf8d49e
Ftopayrespct <122543938+Ftopayrespct@users.noreply.github.com>
2023-01-13 09:35:11 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+basic.showIcon(IconNames.Asleep)
+basic.forever(function () {
+	
+})
